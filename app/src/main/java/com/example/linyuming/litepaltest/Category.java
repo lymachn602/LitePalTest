6d70ec30eb0d2106197f3ea4816808e96672e724
lymachn602 <287643905@qq.com>
2017-05-24 19:43:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/linyuming/litepaltest/Category.java
@@ -0,0 +1,22 @@
+package com.example.linyuming.litepaltest;
+
+/**
+ * Created by linyuming on 2017/5/24.
+ */
+public class Category {
+    private  int id;
+    private String categoryName;
+    private int categoryCode;
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
